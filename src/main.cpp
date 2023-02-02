9c44132e80a43225014295c9fa700272de7a1191
Voyager_lqg7 <stepanenko.vadik@bk.ru>
2023-02-02 14:43:19 +0100

    fixing errors when calling GLAD in CmakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(void)
 
     std::cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << std::endl;
 
-    glClearColor(1, 1, 0, 1);
+    glClearColor(0,1,0,1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
